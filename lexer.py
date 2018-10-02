2979054b8214243e17de55c46d47dfdae0eeb260
Felipe Duitama <felipedc09@gmail.com>
2018-10-02 13:14:57 -0500

    agregar salto de linea

--- lexer.py
+++ lexer.py
@@ -4,12 +4,12 @@ import ply.lex as lex
 
 tokens = [ 'NAME','NUMBER','PLUS','MINUS','TIMES','DIVIDE', 'EQUALS' ]
 
-t_ignore = ' \t \n'
+t_ignore = ' \t'
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
 t_DIVIDE = r'/'
-t_EQUALS = r':='
+t_EQUALS = r'='
 t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
 def t_NUMBER(t):
@@ -17,19 +17,22 @@ def t_NUMBER(t):
     t.value = int(t.value)
     return t
 
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
 # Error handling rule
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
 lex.lex() # Build the lexer
-
+data = ''
 for exp in readLinesFile('expresiones.in'):
-    print("---")
-    print(exp)
-    print("---")
-    lex.input(exp)
-
+    data+=exp
+lex.input(data)
 #lex.input("x = 3 - 4 + 5 * 6")
 while True:
     tok = lex.token()
