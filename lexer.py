507c09faf3c619024bf8c9d0b2d4ed771f00c63f
Felipe Duitama <felipedc09@gmail.com>
2018-10-09 17:12:24 -0500

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -3,36 +3,14 @@ from utilidades import readLinesFile, writeInFile, appendInFile
 import ply.lex as lex
 
 tokens = [ 'NAME','NUMBER','PLUS','MINUS','TIMES','DIVIDE', 'EQUALS' ]
-reserved = {
-    'suma' : 'PLUS',
-    'resta' : 'MINUS',
-    'multiplicacion' : 'TIMES',
-    'division' : 'DIVIDE',
-    
-}
+
 t_ignore = ' \t'
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
 t_DIVIDE = r'/'
 t_EQUALS = r'='
-
-
-def t_NAME(t):
-    r'[a-zA-Z_][a-zA-Z0-9_]*'
-    if t.value.lower() in reserved :
-        t.type = reserved[ t.value.lower() ]
-        t.value = switchValue (t.type)
-    return t
-
-def switchValue(x):
-    return {
-        'PLUS': '+',
-        'MINUS': '-',
-        'TIMES': '*',
-        'DIVIDE': '/',
-        'EQUALS' : '='
-    }[x]
+t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
 def t_NUMBER(t):
     r'\d+'
@@ -43,7 +21,6 @@ def t_NUMBER(t):
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
-
 
 # Error handling rule
 def t_error(t):
@@ -56,7 +33,7 @@ data = ''
 for exp in readLinesFile('expresiones.in'):
     data+=exp
 lex.input(data)
-#lex.input("x = 3 - 4 + 5 * 6")
+
 while True:
     tok = lex.token()
     if not tok: break
